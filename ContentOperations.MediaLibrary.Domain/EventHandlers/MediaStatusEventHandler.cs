53c65dcedfa7abe7f384cd87e1f4e3dd72d27166
Davis Kolakowski <Davis_Kolakowski@comcast.com>
2022-11-10 22:59:07 -0500

    MediaStatusEventHandler

--- ContentOperations.MediaLibrary.Domain/EventHandlers/MediaStatusEventHandler.cs
+++ ContentOperations.MediaLibrary.Domain/EventHandlers/MediaStatusEventHandler.cs
@@ -1,12 +1,23 @@
 ﻿namespace ContentOperations.MediaLibrary.Domain.EventHandlers
 {
+    using ContentOperations.Domain.Core.Bus;
+    using ContentOperations.MediaLibrary.Domain.Events;
+
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class MediaStatusEventHandler
+    public class MediaStatusEventHandler : IEventHandler<MediaStatusCreatedEvent>
     {
+        public MediaStatusEventHandler() 
+        {
+
+        }
+        public Task Handle(MediaStatusCreatedEvent @event)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
